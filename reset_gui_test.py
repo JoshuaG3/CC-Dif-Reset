699ae23caefda37a4d4c4ea25c50eced224dde1f
JoshuaG3 <joshuagould16@gmail.com>
2023-11-16 14:16:24 -0500

    fixed some bugs

--- reset_gui_test.py
+++ reset_gui_test.py
@@ -2,60 +2,24 @@ import pyautogui
 import time
 import keyboard
 import os
+from tkinter import *
 
+root = Tk()
+root.title("Dif Reset Tool")
 
 Username = ("5000")
- 
-
-stores = [2, 3, 5, 7, 8, 12, 18, 19, 25, 28, 30, 32, 40, 51, 54, 60, 67, 68, 70, 77, 81, 84, 85, 86, 93, 94, 96, 103, 104, 106,
-            108, 112, 115, 121, 123, 129, 135, 138, 140, 142, 144, 151, 153, 159, 163, 166, 168, 169, 171, 174, 176, 177, 179,
-            180, 182, 184, 186, 190, 191, 195, 196, 197, 198, 199, 201, 203, 210, 212, 213, 214, 226, 231, 233, 235, 236, 237,
-            239, 242, 243, 246, 247, 249, 250, 251, 252, 254, 262, 267, 270, 271, 274, 280, 283, 286, 287, 290, 291, 292, 304,
-            305, 306, 308, 309, 311, 317, 318, 319, 324, 328, 330, 331, 333, 336, 343, 349, 352, 353, 355, 357, 358, 359, 361, 
-            364, 366, 369, 370, 371, 373, 378, 381, 384, 385, 386, 387, 388, 396, 397, 388, 396, 397, 398, 407, 411, 412, 426,
-            428, 433, 435, 436, 437, 439, 442, 443, 435, 436, 437, 439, 442, 443, 445, 446, 447, 448, 451, 456, 457, 458, 461,
-            464, 473, 478, 479, 481, 483, 487, 488, 493, 494, 496, 498, 500, 501, 504, 506, 507, 509, 510, 511, 517, 526, 527,
-            528, 533, 535, 538, 541, 543, 548, 549, 551, 552, 555, 556, 558, 560, 566, 570, 572, 577, 579, 580, 590, 591, 595,
-            599, 606, 607, 608, 609, 611, 612, 619, 630, 631, 632, 639, 640, 644, 648, 649, 652, 655, 656, 657, 658, 664,
-            671, 672, 681, 683, 684, 687, 697, 698, 701, 702, 705, 708, 710, 711, 713, 720, 721, 726, 728, 729, 736, 737, 741, 
-            743, 745, 750, 751, 757, 761, 767, 768, 773, 777, 1329, 1333, 1404, 1405, 1411, 1412, 1426, 1430, 1432, 1439, 1440,
-            1443, 1444, 1448, 1449, 1461, 1463, 1472, 1478, 1479, 1489, 1500, 1501, 1502, 1504, 1511, 1513, 1537, 1576, 1577, 
-            1581, 1583, 1588, 1590, 1601, 1616, 1617, 1627, 1630, 1631, 1639, 1654, 1655, 1661, 1662, 1667, 1671, 
-            1682, 1688, 1689, 1690, 1692, 1694, 1697, 1710, 1711, 1712, 1715, 1716, 1717, 2187, 2203, 2206, 2207, 2210, 2215, 
-            2219, 2220, 2223, 2225, 2229, 2230, 2234, 2237, 2238, 2244, 2246, 2247, 2249, 2252, 2258, 2261, 2263, 2266, 2269, 
-            2271, 2273, 2274, 2278, 2288, 2304, 2306, 2307, 2309, 2311, 2313, 2315, 2318, 2320, 2325, 2326, 2327, 2328, 2329, 
-            2331, 2333, 2335, 2336, 2337, 2342, 2343, 2348, 2349, 2354, 2355, 2364, 2366, 2367, 2380, 2388, 2394, 2398, 2400, 
-            2404, 2409, 2411, 2413, 2415, 2421, 2422, 2423, 2425, 2427, 2429, 2430, 2433, 2435, 2437, 2439, 2441, 2443, 2444,
-            2445, 2448, 2450, 2454, 2455, 2456, 2459, 2461, 2462, 2465, 2468, 2472, 2474, 2475, 2477, 2480, 2484, 2487, 2488,
-            2491, 2495, 2499, 2501, 2503, 2505, 2507, 2509, 2511, 2515, 2517, 2519, 2521, 2523, 2527, 2529, 2531, 2533, 2545,
-            2548, 2550, 2556, 2557, 2558, 2560, 2562, 2564, 2566, 2567, 2568, 2570, 2571, 2626, 2799]
-
-
-Stores_Input = input("Enter each store number seperated by commas ex(2,18,121): ")  #getting string from user
-password = input("Enter the daily password: ")
-
-os.chdir(r"C:\Users\gouldj\Desktop\Programming Projects\CC automation") #changing the directory to where the images are located
-
-print("String inputed: ", Stores_Input)  #printing what was entered 
-Store_Input_List_String = Stores_Input.split(",")  #spliting the string that was entered into a list of string numbers
-Store_Input_List_Int = []  #creating an empty list for the ints to go into
-for item in Store_Input_List_String:  #converting the strings to ints and adding them into the list
-    Store_Input_List_Int.append(int(item))
-
-
-for n in Store_Input_List_Int:  #running the whole script for each store number in the list that was entered
-    store_num_string = str(n)   #setting n to a string to be used in the script
 
 
 
-    def WDipad_steps_non_liquor():  #Opens WDIpad, enters the store num, clicks SSH and clicks CC
+def WDipad_steps_non_liquor(store_num_string):  #Opens WDIpad, enters the store num, clicks SSH and clicks CC
+        os.chdir(r"C:\Users\gouldj\Desktop\Programming Projects\CC automation")
         try:
             a1, a2 = pyautogui.locateCenterOnScreen('imgs/testwdimg.png', confidence=0.9)   
             pyautogui.click(a1, a2) 
         except:
             print("Exit Code 1")
             exit(1)  
-        time.sleep(1)
+        time.sleep(2)
 
         try:
             b1, b2 = pyautogui.locateCenterOnScreen('imgs/locationnumberimg.png', confidence=0.9) 
@@ -91,7 +55,8 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
 
 
 
-    def Wdipad_steps_liquor():  #Opens the CC for liquor stores from WDIpad
+def Wdipad_steps_liquor(store_num_string):  #Opens the CC for liquor stores from WDIpad
+        os.chdir(r"C:\Users\gouldj\Desktop\Programming Projects\CC automation")
         try:
             a1, a2 = pyautogui.locateCenterOnScreen('imgs/testwdimg.png', confidence=0.9)   
             pyautogui.click(a1, a2) 
@@ -133,7 +98,8 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
 
 
 
-    def logging_into_CC():  #Types the username and password into both screens
+def logging_into_CC():  #Types the username and password into both screens
+        password = password_input.get()
         keyboard.press_and_release('enter') 
         time.sleep(5)
         try:
@@ -167,7 +133,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
 
 
 
-    def find_page_number():  #Finds what page it is on then changes it to page 3
+def find_page_number():  #Finds what page it is on then changes it to page 3
         pyautogui.typewrite("`b", interval=.5)
         time.sleep(2)
         if pyautogui.locateOnScreen('imgs/page1.png', confidence=0.99):
@@ -230,17 +196,17 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
 
 
 
-    def test_page_verify():  #verifies on correct page
+def test_page_verify():  #verifies on correct page
         time.sleep(2)
         if pyautogui.locateOnScreen('imgs/DifService.png', confidence=0.99):
             print("On correct page")
             time.sleep(2)
-            reset_diff()   #when ready change this to  reset_diff()
+            closing_cc()   #when ready change this to  reset_diff()
         else:
             close_cc_8()  
 
 
-    def reset_diff():  #Resets the dif, waits then restarts it
+def reset_diff():  #Resets the dif, waits then restarts it
         keyboard.send('f8') 
         time.sleep(1)
         pyautogui.write("y")
@@ -252,7 +218,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
         closing_cc()
 
 
-    def close_cc_5():  #Logs off and closes the CC from a certain part
+def close_cc_5():  #Logs off and closes the CC from a certain part
         t1, t2 = pyautogui.locateCenterOnScreen('imgs/Xbutton.png', confidence=.9)
         pyautogui.click(t1, t2)
         time.sleep(1)
@@ -268,7 +234,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
         exit(5)
 
 
-    def close_cc_6():  #Logs off and closes the CC from a certain part
+def close_cc_6():  #Logs off and closes the CC from a certain part
         a1, a2 = pyautogui.locateCenterOnScreen('imgs/goback.png', confidence=.9)
         a1 = a1 + 482
         a2 = a2 - 48
@@ -279,7 +245,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
         exit(6)
 
 
-    def close_cc_7():  #Logs off and closes the CC from a certain part
+def close_cc_7():  #Logs off and closes the CC from a certain part
         time.sleep(2)
         keyboard.send('f3')
         time.sleep(1)
@@ -301,7 +267,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
 
 
 
-    def close_cc_8():  #Logs off and closes the CC from a certain part
+def close_cc_8():  #Logs off and closes the CC from a certain part
         time.sleep(2)
         keyboard.send('f3')
         time.sleep(1)
@@ -322,7 +288,7 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
         exit(8)
 
 
-    def closing_cc():  #Logs off and closes the CC from a certain part
+def closing_cc():  #Logs off and closes the CC from a certain part
         print("Closing the CC")
         time.sleep(5)
         keyboard.send('f3')
@@ -342,10 +308,70 @@ for n in Store_Input_List_Int:  #running the whole script for each store number 
         time.sleep(3)
         
 
+def start():
+
+ 
+
+    stores = [2, 3, 5, 7, 8, 12, 18, 19, 25, 28, 30, 32, 40, 51, 54, 60, 67, 68, 70, 77, 81, 84, 85, 86, 93, 94, 96, 103, 104, 106,
+            108, 112, 115, 121, 123, 129, 135, 138, 140, 142, 144, 151, 153, 159, 163, 166, 168, 169, 171, 174, 176, 177, 179,
+            180, 182, 184, 186, 190, 191, 195, 196, 197, 198, 199, 201, 203, 210, 212, 213, 214, 226, 231, 233, 235, 236, 237,
+            239, 242, 243, 246, 247, 249, 250, 251, 252, 254, 262, 267, 270, 271, 274, 280, 283, 286, 287, 290, 291, 292, 304,
+            305, 306, 308, 309, 311, 317, 318, 319, 324, 328, 330, 331, 333, 336, 343, 349, 352, 353, 355, 357, 358, 359, 361, 
+            364, 366, 369, 370, 371, 373, 378, 381, 384, 385, 386, 387, 388, 396, 397, 388, 396, 397, 398, 407, 411, 412, 426,
+            428, 433, 435, 436, 437, 439, 442, 443, 435, 436, 437, 439, 442, 443, 445, 446, 447, 448, 451, 456, 457, 458, 461,
+            464, 473, 478, 479, 481, 483, 487, 488, 493, 494, 496, 498, 500, 501, 504, 506, 507, 509, 510, 511, 517, 526, 527,
+            528, 533, 535, 538, 541, 543, 548, 549, 551, 552, 555, 556, 558, 560, 566, 570, 572, 577, 579, 580, 590, 591, 595,
+            599, 606, 607, 608, 609, 611, 612, 619, 630, 631, 632, 639, 640, 644, 648, 649, 652, 655, 656, 657, 658, 664,
+            671, 672, 681, 683, 684, 687, 697, 698, 701, 702, 705, 708, 710, 711, 713, 720, 721, 726, 728, 729, 736, 737, 741, 
+            743, 745, 750, 751, 757, 761, 767, 768, 773, 777, 1329, 1333, 1404, 1405, 1411, 1412, 1426, 1430, 1432, 1439, 1440,
+            1443, 1444, 1448, 1449, 1461, 1463, 1472, 1478, 1479, 1489, 1500, 1501, 1502, 1504, 1511, 1513, 1537, 1576, 1577, 
+            1581, 1583, 1588, 1590, 1601, 1616, 1617, 1627, 1630, 1631, 1639, 1654, 1655, 1661, 1662, 1667, 1671, 
+            1682, 1688, 1689, 1690, 1692, 1694, 1697, 1710, 1711, 1712, 1715, 1716, 1717, 2187, 2203, 2206, 2207, 2210, 2215, 
+            2219, 2220, 2223, 2225, 2229, 2230, 2234, 2237, 2238, 2244, 2246, 2247, 2249, 2252, 2258, 2261, 2263, 2266, 2269, 
+            2271, 2273, 2274, 2278, 2288, 2304, 2306, 2307, 2309, 2311, 2313, 2315, 2318, 2320, 2325, 2326, 2327, 2328, 2329, 
+            2331, 2333, 2335, 2336, 2337, 2342, 2343, 2348, 2349, 2354, 2355, 2364, 2366, 2367, 2380, 2388, 2394, 2398, 2400, 
+            2404, 2409, 2411, 2413, 2415, 2421, 2422, 2423, 2425, 2427, 2429, 2430, 2433, 2435, 2437, 2439, 2441, 2443, 2444,
+            2445, 2448, 2450, 2454, 2455, 2456, 2459, 2461, 2462, 2465, 2468, 2472, 2474, 2475, 2477, 2480, 2484, 2487, 2488,
+            2491, 2495, 2499, 2501, 2503, 2505, 2507, 2509, 2511, 2515, 2517, 2519, 2521, 2523, 2527, 2529, 2531, 2533, 2545,
+            2548, 2550, 2556, 2557, 2558, 2560, 2562, 2564, 2566, 2567, 2568, 2570, 2571, 2626, 2799]
+
+    Stores_Input = store_input.get()
 
 
-    if n in stores:
-        WDipad_steps_non_liquor()
-    else:
-        Wdipad_steps_liquor()
+    print("String inputed: ", Stores_Input)  #printing what was entered 
+    Store_Input_List_String = Stores_Input.split(",")  #spliting the string that was entered into a list of string numbers
+    Store_Input_List_Int = []  #creating an empty list for the ints to go into
+    for item in Store_Input_List_String:  #converting the strings to ints and adding them into the list
+        Store_Input_List_Int.append(int(item))
 
+    global n
+    for n in Store_Input_List_Int:  #running the whole script for each store number in the list that was entered
+        store_num_string = str(n)   #setting n to a string to be used in the script
+
+        if n in stores:
+            WDipad_steps_non_liquor(store_num_string)
+        else:
+            Wdipad_steps_liquor(store_num_string)
+
+
+
+
+
+#creating the parts of the GUI
+label1 = Label(root, text="Enter store numbers:")
+store_input = Entry(root, width=50, borderwidth=5)
+submit_button = Button(root, text="Reset Dif", command=start)
+label2 = Label(root, text="Enter daily password:")
+password_input = Entry(root, width=50, borderwidth=5)
+
+
+#Placing the different elements onto the GUI
+label1.grid(row=0, column=0, padx=25, pady=5, columnspan=1)
+store_input.grid(row=0, column=1, padx=25, pady=30, columnspan=1)
+submit_button.grid(row=2, column=0,padx=0, pady=20)
+label2.grid(row=1, column=0, padx=25, pady=5, columnspan=1)
+password_input.grid(row=1, column=1, padx=25, pady=30, columnspan=1)
+
+
+
+root.mainloop()
